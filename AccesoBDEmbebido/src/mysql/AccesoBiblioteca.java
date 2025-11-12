162fc6d1f80c4d14f2abcc4bb6beb3e740c062f1
isard <isard@DESKTOP-QS5UVG5>
2025-11-12 21:19:03 +0100

    Acceso a MySql

---
+++ AccesoBDEmbebido/src/mysql/AccesoBiblioteca.java
@@ -0,0 +1,42 @@
+package mysql;
+
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AccesoBiblioteca {
+
+	public static void main(String[] args) {
+
+		
+	 	try {
+	 		
+	 		//1- Crear una instancia del JDBC driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			//2-Especificar la url de la base de datos.
+			//3-Establecer una conexión usando el driver que crea el  objeto Connection.
+			Connection conexion = DriverManager.getConnection("jdbc:mysql://192.168.1.2/biblioteca", "miusuario", "Pass!123456");
+			//4- Crear un objeto Statement, usando Connection.
+            Statement sentencia = (Statement) conexion.createStatement();
+            //5- Armar el postulado SQL y enviarlo a ejecución usando  el Statement.
+            //6- Recibir los resultados en el objeto ResultSet.
+            ResultSet resultado = sentencia.executeQuery("select * from socio");
+            // recorrer el resultado
+            while (resultado.next()) {
+            		System.out.println(resultado.getString("Nombre") + "-" + resultado.getInt("Codigo"));
+            }
+            
+            //cerrar la conexion
+            conexion.close();
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
